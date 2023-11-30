f09d31ddbe3841d40d58ba5490f2314f68f19327
s <132883472+Susanta37@users.noreply.github.com>
2023-11-30 05:13:28 +0000

    file added

---
+++ BasicJS/Comparision.js
@@ -0,0 +1,8 @@
+console.log(2>1);
+console.log(2>=19);
+console.log(2<1);
+console.log('2'>48);/*In JavaScript, when you use the greater than (>) operator to compare values, the comparison is done lexicographically (based on the Unicode values of the characters) rather than numerically if at least one of the operands is a string.
+In the expression console.log('2' > 48);, you are comparing the string '2' with the number 48. When comparing a string to a number, JavaScript will attempt to convert the string to a number before making the comparison.*/
+console.log('02'>1)
+console.log(null==0);
+console.log(undefined==0);
