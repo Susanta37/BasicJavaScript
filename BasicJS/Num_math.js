042148ff450ac88f67862243672e08921fbb4ca6
s <132883472+Susanta37@users.noreply.github.com>
2023-12-04 05:35:33 +0000

    file added

---
+++ BasicJS/Num_math.js
@@ -0,0 +1,17 @@
+//toPrecision() is used for  return fixed point notation
+const otherNum=125.8920
+console.log(otherNum.toPrecision(2))
+//--------------------------------------------
+//toLocaleString ()  used for convert value to indian rupees format
+ const rupees=12453225
+ console.log(rupees.toLocaleString())
+ //---------------------------------------------
+ console.log(Math)
+ console.log(Math.abs(-144))// return absolute value means always positive(144
+ console.log(Math.round(12.5))// return round value
+ console.log(Math.ceil(4.1)) // return the heighest number(maximum) =5
+ console.log(Math.floor(4.8))// return the lowest or minimum =4
+ console.log(Math.random()) // give random value between 0 - 1
+ max=20
+ min=4
+ console.log(Math.floor(Math.random()*max-min+1)+min);
